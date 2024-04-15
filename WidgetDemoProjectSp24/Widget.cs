eedc42a3ebe56c9a609fc8f6ebdbb8a5a6a738e1
Student <Student@h344-25T>
2024-04-15 12:03:44 -0400

    Add project files.

---
+++ WidgetDemoProjectSp24/Widget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetDemoProjectSp24
+{
+    internal class Widget
+    {
+        // constructor - must be public and no return type
+        public Widget(string name, string color) {
+            WidgetName = name;
+            Color = color;
+        
+        }
+
+
+        public string WidgetName { get; set; }  
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
